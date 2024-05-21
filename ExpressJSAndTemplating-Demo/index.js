0c2e6ae2d6f861e24de7f12e85d338d958a02d7b
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-21 21:41:09 +0200

    ExpressJS and Templating

---
+++ ExpressJSAndTemplating-Demo/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const app = express();
+
+
+
+app.listen(7000, () => console.log('Server is listening on port 7000.'));
