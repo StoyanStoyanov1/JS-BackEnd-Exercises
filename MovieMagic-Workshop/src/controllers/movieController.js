a5d762b80e687d2a31c416a2bdfd9658665a564c
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-22 15:01:37 +0200

    movie detail

--- MovieMagic-Workshop/src/controllers/movieController.js
+++ MovieMagic-Workshop/src/controllers/movieController.js
@@ -7,10 +7,18 @@ router.get('/create', (req, res) => {
 
 router.post('/create', (req, res) => {
 	const newMovie = req.body;
+	newMovie.ratingStars = '&#x2605;'.repeat(newMovie.rating);
 
 	moviesService.create(newMovie);
 
 	res.redirect('/')
+});
+
+router.get('/movies/:movieId', (req, res) => {
+	const movieId = req.params.movieId;
+	const movie = moviesService.getOne(movieId);
+
+	res.render('details', {movie});
 })
 
 module.exports = router;
--- MovieMagic-Workshop/src/services/movieService.js
+++ MovieMagic-Workshop/src/services/movieService.js
@@ -1,11 +1,16 @@
-const movies = [{
-
-}];
+const movies = [];
 
 exports.getAll = () => {
 	return movies.slice();
 }
 
 exports.create = (movieData) => {
+	movieData._id = movies.length + 1;
 	movies.push(movieData)
+}
+
+exports.getOne = (movieId) => {
+	const movie = movies.find(movie => movie._id == movieId);
+
+	return movie;
 }
