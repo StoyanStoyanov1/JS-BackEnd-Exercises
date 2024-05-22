ace3088f7d6b321c1462429d3edeed6a3cff3413
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-22 10:21:06 +0200

    create movie and 404

--- MovieMagic-Workshop/src/controllers/homeController.js
+++ MovieMagic-Workshop/src/controllers/homeController.js
@@ -4,4 +4,11 @@ router.get('/', (req, res) => {
 	res.render('home')
 });
 
+router.get('/about', (req, res) => {
+	res.render('about')
+})
+
+router.get('*', (req, res) => {
+	res.render('404')
+})
 module.exports = router;
--- MovieMagic-Workshop/src/routes.js
+++ MovieMagic-Workshop/src/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
+const movieController = require('./controllers/movieController')
 
+router.use(movieController);
 router.use(homeController);
 
 module.exports = router;
