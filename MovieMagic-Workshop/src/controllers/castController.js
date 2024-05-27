7a9848edf26c368b8859a53aa678393f7c7dba11
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-27 12:47:27 +0200

    add a cast Controller

---
+++ MovieMagic-Workshop/src/controllers/castController.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const castService = require('../services/castService');
+
+router.get('/create', (req, res) => {
+	res.render('cast/create')
+});
+
+router.post('/create', async (req, res) => {
+	const castData = req.body;
+
+	await  castService.create(castData);
+
+	res.redirect('/');
+});
+
+module.exports = router;
