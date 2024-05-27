db1ef0cca0b310f00f0b0836089f2cfe2c63cb40
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-27 09:35:17 +0200

    model cast

---
+++ MovieMagic-Workshop/src/models/Cast.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const castSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	age: {
+		type: Number,
+		required: true,
+		max: 120,
+		min: 14,
+	},
+	born: {
+		type: String,
+		required: true,
+	},
+	nameInMovie: {
+		type: String,
+		required: true,
+	},
+	castImage: {
+		type: String,
+		required: true,
+		validate: {
+			validator: (value) => {
+				return /^https?:\/\//.test(value);
+			},
+			message: (props) => `${props.value} is invalid url for the castImage!`}
+	},
+});
+
+const Cast = mongoose.model('Cast', castSchema);
+
+module.exports = Cast;
