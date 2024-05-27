0332598b88489b4cf576bc329f8df653e74543ad
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-27 12:50:32 +0200

    update routes

--- MovieMagic-Workshop/src/routes.js
+++ MovieMagic-Workshop/src/routes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
-const movieController = require('./controllers/movieController')
+const movieController = require('./controllers/movieController');
+const castController = require('./controllers/castController');
 
 router.use(movieController);
 router.use(homeController);
+router.use('/cast', castController)
+
+router.get('*', (req, res) => {
+	res.redirect('/404');
+});
 
 module.exports = router;
