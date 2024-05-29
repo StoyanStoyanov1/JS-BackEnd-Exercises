373ec48a587de58a973a71b87b7aab5ecc5c1f8b
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-29 09:23:06 +0200

    register controller

--- Projects/CourseBook/routes.js
+++ Projects/CourseBook/routes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
-router.get('/', (req, res) => {
-	res.send('Hello world');
-})
+
+const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
+
+router.use(homeController);
+router.use('/auth', authController);
+
 module.exports = router;
