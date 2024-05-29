1b906d2d2a91c48870eee7ed4008d2faf552e9f7
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-29 18:28:23 +0200

    jsonwebtoken

--- Projects/CourseBook/controllers/authController.js
+++ Projects/CourseBook/controllers/authController.js
@@ -14,4 +14,14 @@ router.post('/register', async (req, res) => {
 	res.redirect('/auth/login');
 });
 
+router.get('/login', (req, res) => {
+	res.render('auth/login');
+})
+
+router.post(/login/, async (req, res) => {
+	const loginData = req.body;
+
+	await authService.login(loginData);
+})
+
 module.exports = router;
--- Projects/CourseBook/services/authService.js
+++ Projects/CourseBook/services/authService.js
@@ -1,5 +1,40 @@
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jsonwebtoken')
+
+
 const User = require('../models/User');
 
-exports.register = (userData) => User.create(userData);
+exports.register = async (userData) => {
+	if (userData.password !== userData.rePassword) {
+		throw new Error('Password mismatch');
+	}
 
+	const user = await User.findOne({email: userData.email});
+	if (user) {
+		throw new Error('User already exists');
+	}
 
+	User.create(userData)
+};
+
+exports.login = async ({email , password}) => {
+	//Get user from db
+	const user = await User.findOne({email});
+
+	if (!user) {
+		throw new Error('Username or password is invalid');
+	}
+	// Check password
+	const isValid = await bcrypt.compare(password, user.password)
+
+	if (!isValid) {
+		throw new Error('Username or password is invalid');
+	}
+
+	// generate Token
+
+	await jwt.sign();
+
+	// return token
+
+}
