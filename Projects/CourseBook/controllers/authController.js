94ac171d162dcfa09186e4f890b4965179afa7ec
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-30 08:10:40 +0200

    error handling

--- Projects/CourseBook/controllers/authController.js
+++ Projects/CourseBook/controllers/authController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const authService = require('../services/authService');
+const {getErrorMessage} = require("../utils/errorUtils");
 
 router.get('/register', (req, res) => {
 	res.render('auth/register');
@@ -9,11 +10,15 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
 	const userData = req.body;
 
-	const token = await authService.register(userData);
+	try {
+		const token = await authService.register(userData);
 
-	res.cookie('auth', token);
+		res.cookie('auth', token);
 
-	res.redirect('/');
+		res.redirect('/');
+	} catch (err) {
+		res.render('auth/login', {error: getErrorMessage(err)});
+	}
 });
 
 router.get('/login', (req, res) => {
@@ -23,10 +28,14 @@ router.get('/login', (req, res) => {
 router.post(/login/, async (req, res) => {
 	const loginData = req.body;
 
-	const token = await authService.login(loginData);
+	try {
+		const token = await authService.login(loginData);
 
-	res.cookie('auth', token);
-	res.redirect('/');
+		res.cookie('auth', token);
+		res.redirect('/');
+	} catch (err) {
+		res.render('auth/login', {error: getErrorMessage(err)});
+	}
 });
 
 router.get('/logout', (req, res) => {
---
+++ Projects/CourseBook/utils/errorUtils.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+exports.getErrorMessage = (err) => {
+	if (err instanceof mongoose.Model) {
+		return Object.values(err.errors).at(0).message;
+	} else if (err instanceof Error){
+		return err.message;
+	}
+}
