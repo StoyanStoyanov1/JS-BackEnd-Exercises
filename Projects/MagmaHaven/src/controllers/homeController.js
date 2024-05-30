fcb3766c09e05abd68d60521078262a1a614d57c
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-30 13:04:42 +0200

    viwes

---
+++ Projects/MagmaHaven/src/controllers/homeController.js
@@ -0,0 +1,6 @@
+
+const homeController = (req, res) => {
+	res.render('home');
+};
+
+module.exports = homeController;
--- Projects/MagmaHaven/src/index.js
+++ Projects/MagmaHaven/src/index.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 const handlebars = require('express-handlebars');
-
 const app = express();
+const routes = require('./routes');
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }));
@@ -11,5 +15,6 @@ app.engine('hbs', handlebars.engine({
 }));
 
 app.set('view engine', 'hbs');
+app.use(routes);
 
 app.listen(3000, () => console.log('Server is running on port http://localhost:3000'));
