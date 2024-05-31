309fa29facf0e3bad881745c39834e2363c24588
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-31 08:14:01 +0200

    auht middleware

---
+++ Projects/MagmaHaven/src/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+const {SECRET} = require('../config');
+
+exports.authMiddleware = async (req, res, next) => {
+	const token = req.cookie['auth'];
+
+	if (!token) {
+		return next();
+	}
+
+	try {
+		const decodedToken = await jwt.verify(token, SECRET);
+
+		req.user = decodedToken;
+		res.locals.sAuthenticated = true;
+		res.locals.user = decodedToken;
+
+		next();
+	} catch (err) {
+		res.clearCookie('auth');
+		res.redirect('/auth/login');
+	}
+}
+
+exports.isAuth = (req, res, next) => {
+	if (!req.user) {
+		res.redirect('auth/login');
+	}
+
+	next();
+}
