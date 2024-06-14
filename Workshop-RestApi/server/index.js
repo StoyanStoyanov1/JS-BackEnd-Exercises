77520a9261d6af0ffb4bbc198da8cf8efeae5877
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-06-14 09:02:16 +0200

    cors

--- Workshop-RestApi/server/index.js
+++ Workshop-RestApi/server/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.json({
+		message: 'Hello'
+	})
+})
 
 app.listen(5000, () => console.log('Listening on port 5000'));
